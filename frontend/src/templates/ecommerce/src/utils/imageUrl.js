5cee5036f6ec519b7ad34af257b703da6a7ea16f
savannaik090-coder <savannaik090@gmail.com>
2026-03-14 11:12:32 +0000

    Revert "Organize storefront templates into a dedicated directory structure"
    This reverts commit 9611a1238d94e2f4b4870e287056abba65957dc0.

--- frontend/src/templates/ecommerce/src/utils/imageUrl.js
+++
@@ -1,9 +0,0 @@
-import { getApiUrl } from '../services/api.js';
-
-export function resolveImageUrl(src) {
-  if (!src) return '';
-  if (src.startsWith('data:') || src.startsWith('http://') || src.startsWith('https://') || src.startsWith('blob:')) {
-    return src;
-  }
-  return getApiUrl(src);
-}
