ca35305f1d938c453f1c5d02aac75f2a49b1fd41
savannaik090-coder <savannaik090@gmail.com>
2026-04-17 18:47:32 +0000

    Skjs

--- backend/config.js
+++ backend/config.js
@@ -3,3 +3,5 @@ export const PLATFORM_URL = `https://${PLATFORM_DOMAIN}`;
 export const SUPPORT_EMAIL = `support@${PLATFORM_DOMAIN}`;
 export const FROM_EMAIL = `noreply@${PLATFORM_DOMAIN}`;
 export const VAPID_SUBJECT = `mailto:noreply@${PLATFORM_DOMAIN}`;
+
+
